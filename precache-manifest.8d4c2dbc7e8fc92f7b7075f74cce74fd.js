2ece6d3fabb412c186c51a3c13d5adc7919569a9
Kevin Huang <khuan028@ucr.edu>
2018-12-31 01:39:59 -0800

    Updates

---
+++ precache-manifest.8d4c2dbc7e8fc92f7b7075f74cce74fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ab14f44a743f92e22b0e887a1ee524e",
+    "url": "/NetNinjaTutorial/static/media/mario-bg.0ab14f44.png"
+  },
+  {
+    "revision": "f8af5e8e0818a121a794",
+    "url": "/NetNinjaTutorial/static/js/runtime~main.f8af5e8e.js"
+  },
+  {
+    "revision": "7d6a665fc3bd564c34e5",
+    "url": "/NetNinjaTutorial/static/js/main.7d6a665f.chunk.js"
+  },
+  {
+    "revision": "0ba314ad5f571d7f5a99",
+    "url": "/NetNinjaTutorial/static/js/1.0ba314ad.chunk.js"
+  },
+  {
+    "revision": "7d6a665fc3bd564c34e5",
+    "url": "/NetNinjaTutorial/static/css/main.c0dd67a6.chunk.css"
+  },
+  {
+    "revision": "6a359f6bc5ed83bc27ecdc9fec4c9635",
+    "url": "/NetNinjaTutorial/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.3883695cd809ab0279d8d769de139fcd.js"
+  "/NetNinjaTutorial/precache-manifest.8d4c2dbc7e8fc92f7b7075f74cce74fd.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/NetNinjaTutorial/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
