b17b34070030585780ef06b16ecf2f3f44a440ba
Kevin Huang <khuan028@ucr.edu>
2018-12-30 21:54:06 -0800

    Updates

---
+++ precache-manifest.db86d22fce4d72487c6df2d60169027f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f8af5e8e0818a121a794",
+    "url": "/NetNinjaTutorial/static/js/runtime~main.f8af5e8e.js"
+  },
+  {
+    "revision": "a8520035fe0bc1ce7fec",
+    "url": "/NetNinjaTutorial/static/js/main.a8520035.chunk.js"
+  },
+  {
+    "revision": "553f6926fb6d5c53035b",
+    "url": "/NetNinjaTutorial/static/js/1.553f6926.chunk.js"
+  },
+  {
+    "revision": "a8520035fe0bc1ce7fec",
+    "url": "/NetNinjaTutorial/static/css/main.1b63e3f7.chunk.css"
+  },
+  {
+    "revision": "e72a51fcdfb4b30b562107b971303694",
+    "url": "/NetNinjaTutorial/index.html"
+  }
+];
